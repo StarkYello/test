ab7b20d9d54a1af7fca10675a8925a18620cff4a
Xing Huang <thenamexing@ufl.edu>
2017-05-08 17:43:01 -0400

    changed

--- app/src/main/java/com/example/xing/test/MainActivity.java
+++ app/src/main/java/com/example/xing/test/MainActivity.java
@@ -57,7 +57,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    public void runtest(){
-        Log.d("test","hello world");
-    }
+
 }
