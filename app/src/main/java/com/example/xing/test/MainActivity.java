83d3988d1dcd87968bd103abebb060d16c4c5572
Xing Huang <thenamexing@ufl.edu>
2017-05-08 16:37:19 -0400

    changed

--- app/src/main/java/com/example/xing/test/MainActivity.java
+++ app/src/main/java/com/example/xing/test/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -55,5 +56,8 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    private void test(){
+        Log.d("testing","test");
     }
 }
