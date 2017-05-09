a972fb83d98e5a8ceb8aaeafb01df9e0112885fe
Xing Huang <thenamexing@ufl.edu>
2017-05-09 18:20:37 -0400

    changed

--- app/src/main/java/com/example/xing/test/ExperimentActivity.java
+++ app/src/main/java/com/example/xing/test/ExperimentActivity.java
@@ -2,6 +2,12 @@ package com.example.xing.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class ExperimentActivity extends AppCompatActivity {
 
@@ -9,5 +15,19 @@ public class ExperimentActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_experiment);
+        final Animation animation = new AlphaAnimation(1, 0); // Change alpha from fully visible to invisible
+        animation.setDuration(100); // duration - half a second
+        animation.setInterpolator(new LinearInterpolator()); // do not alter animation rate
+        animation.setRepeatCount(Animation.INFINITE); // Repeat animation infinitely
+        animation.setRepeatMode(Animation.REVERSE); // Reverse animation at the end so the button will fade back in
+        final ImageView btn = (ImageView) findViewById(R.id.imageView3);
+        btn.startAnimation(animation);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View view) {
+                view.clearAnimation();
+            }
+        });
     }
+
 }
